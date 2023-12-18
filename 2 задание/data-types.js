05be482e98e13109f7860ddd1f1702df6223c554
Irina2332 <149126825+Irina2332@users.noreply.github.com>
2023-12-19 03:29:34 +1300

    Add files via upload

---
+++ 2 задание/data-types.js
@@ -0,0 +1,23 @@
+let age = "Irina";
+console.log("age", age, typeof age);
+
+let bool = 3 < 2;
+console.log("bool", bool, typeof bool);
+
+let num = 19;
+console.log("num", num, typeof num);
+
+let nul = null;
+console.log("nul", nul, typeof nul);
+
+let unde = undefined;
+console.log("unde", unde, typeof unde);
+
+let obj = new Object();
+console.log("obj", obj, typeof obj);
+
+let big = BigInt(19);
+console.log("big", big, typeof big);
+
+let symb = Symbol("3");
+console.log("symb", symb, typeof symb);
---
+++ 2 задание/index.js
@@ -0,0 +1,11 @@
+let age = "Irina";
+age = 19;
+alert(age);
+
+let bool = 3 < 2;
+bool = null;
+alert(bool);
+
+let num = 19;
+num = "19";
+alert(num);
